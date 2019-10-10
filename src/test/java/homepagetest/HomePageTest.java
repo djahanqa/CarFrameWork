dd858b18f49fb66959e25ec289697fe650b87c7f
dilrubajahan <djahanqa@gmail.com>
2019-10-09 23:04:35 -0400

    Car WebPage Project's home page.

--- src/test/java/homepagetest/HomePageTest.java
+++ src/test/java/homepagetest/HomePageTest.java
@@ -1,4 +1,34 @@
 package homepagetest;
 
-public class HomePageTest {
+import base.ScriptBase;
+import controller.HomePage;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class HomePageTest extends ScriptBase {
+    HomePage homePage;
+
+    @BeforeTest
+    public void beforetest() {
+        init();
+    }
+
+    @Test
+    public void verifylogo(){
+        homePage=new HomePage(driver);
+         homePage.companylogo();
+
+    }
+
+    @AfterTest
+    public void closebrowser() {
+        driver.close();
+        driver.quit();
+
+
+    }
 }
+
+
